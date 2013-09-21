e2f8faa389bebcbab43ba9e478d3e81a28a25a45
Lee Avital <leeavital@gmail.com>
2013-09-21 04:50:15 -0400

    Half functional authentication controller
    New route in flaskr (current_user)

--- flaskr.py
+++ flaskr.py
@@ -59,8 +59,9 @@ def get_ingredients():
 	print "\n\n\n"
 
 	ingredients = [(x['name'], x['type']) for x in cur.fetchall()] 
-
+	
 	return render_template('ingredients.html',  **{'ingredients': ingredients})
+
 
 @app.route('/recipe/<int:id>')
 def view_recipe(id):
@@ -78,6 +79,64 @@ def recommendations():
    # can't use flask.jsonify because it won't take a list
    return flask.Response(json.dumps( dummy ),  mimetype='application/json')
 
+
+@app.route('/current_user')
+def current_user():
+   """if the session is logged in, return an object containing the user
+      data. Else return an empty object"""
+   
+   response = None  
+   r_code = 200  
+   d = {}
+
+   if "username" in session:
+	  d = {"username": request.session.username}
+   else:
+	  d = {}
+	  r_code = 401 # unauthorized 401
+
+   response = flask.jsonify( d )
+   response.headers[ "Status" ] = r_code 
+   return response
+
+
+@app.route("/login", methods=["POST"])
+def login():
+   try_pword = request.form["password"]
+   
+   r_code = 200
+   d = {}
+
+   query = """SELECT COUNT(*) AS matched FROM users WHERE password=MD5("%s")""" % try_pword
+   cur = g.db.cursor()
+   cur.execute(query)
+   
+   num_matches = cur.fetchone()["matched"]
+   
+   if num_matches == 0:
+	  r_code = 401 
+	  d = {"error": "bad password"}
+   else:
+	  r_code = 401  # unauthorized
+	  d = {"status": "ok"}
+
+
+   response = flask.jsonify( d )
+   response.headers["status"] = r_code
+
+   return response
+   
+
+@app.route("/user_create", methods=["POST"])
+def user_create():
+   uname = request.form["username"]
+   password = request.form["password"]
+
+   query = """INSERT INTO users(username, password) VALUES("%s", MD5("%s"))""" % (uname, password)
+   print query
+
+   return query
+	  
 
 if __name__ == '__main__':
 	app.run()
