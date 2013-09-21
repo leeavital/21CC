899b03e2613c83b3fe80061c2376c1dca7d482a7
Ben Cohen <bn.chn2@gmail.com>
2013-09-21 12:47:32 -0400

    changes to db maker

--- ingredients/databaseutil.py
+++ ingredients/databaseutil.py
@@ -13,7 +13,7 @@ def createIngredients():
 def createRecipes():
 	db = MySQLdb.connect(host="ec2-54-219-48-12.us-west-1.compute.amazonaws.com",user="test_user",passwd="mypass",db="prod")
 	cur = db.cursor()
-	cur.execute('CREATE TABLE IF NOT EXISTS recipes (id INT AUTO_INCREMENT, name CHAR(30) UNIQUE, votes INT NOT NULL, salty INT NOT NULL, sweet INT NOT NULL, spicy INT NOT NULL, savory INT NOT NULL, filling INT NOT NULL, cuisine CHAR(30) NOT NULL, PRIMARY KEY (id))')
+	cur.execute('CREATE TABLE IF NOT EXISTS recipes (id INT AUTO_INCREMENT, name CHAR(30) UNIQUE, votes INT NOT NULL, salty INT NOT NULL, sweet INT NOT NULL, spicy INT NOT NULL, savory INT NOT NULL, filling INT NOT NULL, cuisine CHAR(30) NOT NULL, meal CHAR(30) NOT NULL, PRIMARY KEY (id))')
 
 def createUsers():
 	db = MySQLdb.connect(host="ec2-54-219-48-12.us-west-1.compute.amazonaws.com",user="test_user",passwd="mypass",db="prod")
@@ -32,7 +32,19 @@ def createRecipecombo():
 def populate():
 	db = MySQLdb.connect(host="ec2-54-219-48-12.us-west-1.compute.amazonaws.com",user="test_user",passwd="mypass",db="prod", cursorclass=MySQLdb.cursors.DictCursor)
 	cur = db.cursor()
-	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine) VALUES ('moms_spagetti','200','10','10','10','10','10','super')")
+
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Chocolate Cake','1','2','9','1','3','5','American', 'Desert')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Apple Pie','1','2','8','1','5','5','American', 'Desert')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Steamed Broccoli','1','2','2','1','2','3','Vegetable', 'Side')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Creamed Corn','1','3','5','1','6','4','Vegetable', 'Side')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Steak','1','4','2','2','8','8','American', 'Main')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Falafel','1','6','3','7','7','7','Mediterranean', 'Main')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Lentils','1','6','2','1','6','4','Mediterranean', 'Side')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Hot Sauce','1','6','2','10','3','1','American', 'Side')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Spaghetti Marinara','1','6','5','3','7','7','Mediterranean', 'Main')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Potato Chips','1','7','2','1','3','3','American', 'Main')")
+	cur.execute("INSERT INTO recipes (name,votes,salty,sweet,spicy,savory,filling,cuisine, meal) VALUES ('Lox Omelette','1','8','4','1','5','5','American', 'Breakfast')")
+
 	cur.execute("INSERT INTO users (name,password) VALUES ('Ben', md5('benlovesboys')),('Sean',md5('kettle'))")
 	cur.execute("INSERT INTO ingredients (name, type) VALUES ('salt','spice'),('pepper','spice'),('spagetti','starch'),('scott_tenormans_parents','long pig'),('tomato_sauce','sauce')")
 	cur.execute("SELECT id FROM recipes WHERE name = 'moms_spagetti'")
